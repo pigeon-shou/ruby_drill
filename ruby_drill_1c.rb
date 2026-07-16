7b91425842150c28234e2be612e54d13bf06a03a
pigeon-shou <pigeonshow2024@gmail.com>
2026-07-16 23:57:10 +0900

    chars stlip

---
+++ ruby_drill_1c.rb
@@ -0,0 +1,17 @@
+hash = { one: 1, two: 2, three: 3}
+puts hash.keys
+puts hash.values
+
+array = ["田中", "佐藤", "山本"]
+puts array[0]
+
+# キー:　バリューの順
+# 配列は0から
+
+str = "abcde"
+puts str.split("")
+# 配列化
+str = "a-b-c-d-e"
+result = str.split("-")
+puts result
+puts str.chars
